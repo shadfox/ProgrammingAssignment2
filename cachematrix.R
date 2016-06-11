db8231cba1c4319d551fd3e9ff5571bbc9ab7e15
Thiago Amanajas <ihgtao@gmail.com>
2016-06-12 00:28:09 +0100

    Cache inverted matrix solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the values is always better than calculate it everytime
+## These methods are used to cache a matrix and its inverse
 
-## Write a short comment describing this function
+## Cache the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invMatrix <- NULL
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        invMatrix <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(newInverseMatrix) invMatrix <<- newInverseMatrix
+    getInverse <- function() invMatrix
+    list(set=set,
+         get=get,
+         setInverse=setInverse,
+         getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+## The method solve the matrix and cache it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invMatrix <- x$getInverse()
+    if(!is.null(invMatrix)) {
+        message("getting cached data.")
+        return(invMatrix)
+    }
+    data <- x$get()
+    invMatrix <- solve(data)
+    x$setInverse(invMatrix)
+    return(invMatrix)
 }
+
+
+
+## How to use it:
+
+# > source("cachematrix.R")
+# > x = rbind(rnorm(2), rnorm(2))
+# > m = makeCacheMatrix(x)
+# > m$get()
+# [,1]       [,2]
+# [1,] -0.4047107 -1.5279713
+# [2,]  0.1947928 -0.2092455
+# > cacheSolve(m)
+# [,1]      [,2]
+# [1,] -0.5473023  3.996559
+# [2,] -0.5094997 -1.058561
+# > cacheSolve(m)
+# getting cached data.
+# [,1]      [,2]
+# [1,] -0.5473023  3.996559
+# [2,] -0.5094997 -1.058561
